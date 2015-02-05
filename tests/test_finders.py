9493b2f400d0290e6ba7363a509b6079d37cac0b
André Ericson <de.ericson@gmail.com>
2015-02-05 00:34:40 -0300

    Mock the backend in test_finders

--- tests/test_finders.py
+++ tests/test_finders.py
@@ -4,8 +4,19 @@ from django.test import TestCase
 from django.core.management import call_command
 
 from model_mommy import mommy
+from mock import patch
 
 from cdn_js.models import CDNFile
+
+
+class FakeBackend(object):
+
+    def list_package_files(self, *args, **kwargs):
+        return ['jquery.js', 'jquery.min.js', 'jquery.min.map']
+
+    def get_file_url(self, package, version, filename):
+        return ('http://cdn.jsdelivr.net/' + package + '/' + version + '/' +
+                filename)
 
 
 class CDNFinderTestCase(TestCase):
@@ -15,18 +26,15 @@ class CDNFinderTestCase(TestCase):
         call_command('bower', 'install')
         # create some garbage
         mommy.make(CDNFile, _quantity=3)
-        call_command('collectstatic', interactive=False)
+        with patch('cdn_js.backends.cdn_jsdelivr', FakeBackend()):
+            call_command('collectstatic', interactive=False)
 
     def test_jquery_links_are_saved(self):
-        self.assertTrue(
-            CDNFile.objects.filter(
-                url='http://cdn.jsdelivr.net/jquery/2.1.3/jquery.js').exists())
-        self.assertTrue(
-            CDNFile.objects.filter(
-                url='http://cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js').exists())
-        self.assertTrue(
-            CDNFile.objects.filter(
-                url='http://cdn.jsdelivr.net/jquery/2.1.3/jquery.min.map').exists())
+        fake_backend = FakeBackend()
+        for filename in fake_backend.list_package_files():
+            self.assertTrue(CDNFile.objects.filter(
+                            url=fake_backend.get_file_url('jquery', '2.1.3',
+                                                          filename)))
 
     def test_clean_up(self):
         self.assertEquals(CDNFile.objects.count(), 3)
